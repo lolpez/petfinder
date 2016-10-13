0d518f8023025ff9cf6b529212b1f776fe4f1528
lulax666 <luiyicpu@hotmail.com>
2016-10-13 16:17:09 -0400

    version 6

--- libs/View/PosterVer.tpl.php
+++ libs/View/PosterVer.tpl.php
@@ -66,7 +66,7 @@ $this->display('_Header.tpl.php');
             </ul>
             <div class="tab-content p m-b-md b-t b-t-2x">
                 <div role="tabpanel" class="tab-pane animated fadeIn active" id="tab_1">
-                    sup
+                    <div id='googleMap' style='width:400px;height:400px;margin: auto'></div>
                 </div>
                 <div role="tabpanel" class="tab-pane animated fadeIn" id="tab_2">
                     bitches
@@ -75,6 +75,60 @@ $this->display('_Header.tpl.php');
         </div>
     </div>
 </div>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4-LpGRhQvBCmeuEcv6UqBdpu6-8SyDMk"></script>
+<script>
+    $(document).ready(function() {
+        InicializarMapa(<?php echo $this->poster->Latitud ?>, <?php echo $this->poster->Longitud ?>);//Inicializar mapa con la ciudad de La Paz por defecto
+    });
+
+    function InicializarMapa(latitud,longitud){
+        var infowindow = new google.maps.InfoWindow();
+
+        var mapProp = {
+            center:new google.maps.LatLng(latitud,longitud),
+            zoom:15,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+        var marker = new google.maps.Marker({
+            position:{lat: latitud, lng: longitud},
+            map:map,
+            draggable:false
+        });
+
+        // Add circle overlay and bind to marker
+        var circle = new google.maps.Circle({
+            map: map,
+            radius: 200,    // distancia en metros
+            fillColor: '#AA0000'
+        });
+        circle.bindTo('center', marker, 'position');
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+
+        google.maps.event.addListener(marker,'dragend',function(){
+            setLatLong(infowindow,map,marker,marker.getPosition().lat(), marker.getPosition().lng());
+        });
+
+        google.maps.event.addListener(map, 'click', function(event) {
+            //marker.setPosition(event.latLng);
+            //setLatLong(infowindow,map,marker,event.latLng.lat(),event.latLng.lng());
+        });
+        setLatLong(infowindow,map,marker,marker.getPosition().lat(), marker.getPosition().lng());
+    }
+
+    function setLatLong(infowindow, map, marker, latitud, longitud){
+        infowindow.setContent('Latitud: ' + latitud + '<br>Longitud: ' + longitud);
+        infowindow.open(map,marker);
+        $("#latitud").attr("value", latitud);
+        $("#longitud").attr("value", longitud);
+    }
+</script>
 
 <?php
 $this->display('_Footer.tpl.php');
